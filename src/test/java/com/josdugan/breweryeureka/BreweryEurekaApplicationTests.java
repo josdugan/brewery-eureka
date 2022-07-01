06f16e72ca09ab22912374b8cb5d321e634267a8
josdugan <josdugan@gmail.com>
2022-07-01 14:09:34 -0400

    Initial commit

---
+++ src/test/java/com/josdugan/breweryeureka/BreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdugan.breweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
